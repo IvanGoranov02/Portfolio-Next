bb2456db072bf2cc2d574313f9328e3efa4ba18f
IvanGoranov02 <ivan.goranov@hight.io>
2025-02-27 17:47:47 +0200

    fix: fast fix

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center relative top-3">
+    <div className="flex justify-center items-center fixed top-3 w-full">
       <nav className="flex items-center gap-1 p-0.5 border-white/15 rounded-full border bg-white/10 backdrop-blur">
         <a className="nav-item" href="#">
           Home
