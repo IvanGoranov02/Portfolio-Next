8ae53cf62b118388f4d7b5aeceab716b8e82061c
IvanGoranov02 <ivan.goranov@hight.io>
2025-03-01 15:17:03 +0200

    feat: Add Projects section with responsive design and project showcase

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,10 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
@@ -42,5 +46,78 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase text-sm tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text ">
+            Real-world Results
+          </p>
+        </div>
+
+        <h2 className="text-3xl font-serif md:text-5xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-white/60 text-center mt-4 md:text-lg max-w-md mx-auto lg:text-xl">
+          See how I transformed concepts into engaging digital experiences.
+        </p>
+        <div className="flex flex-col mt-10 gap-20 md:mt-20">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 
+              overflow-hidden after:z-10 after:content-['']  
+              after:absolute after:inset-0 after:outline after:outline-2 
+              after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none lg:px-20 lg:pt-16"
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{ backgroundImage: `url(${grainImage.src})` }}
+              ></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase text-sm gap-2 tracking-widest text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span className="text-white/60 mx-2">&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <div>
+                    <h3 className="text-2xl font-serif mt-4 md:mt-5 md:text-4xl">
+                      {project.title}
+                    </h3>
+                    <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                    <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                      {project.results.map((result) => (
+                        <li
+                          className="flex gap-2 text-sm text-white/50 md:text-base"
+                          key={result.title}
+                        >
+                          <CheckCircleIcon className="size-5 md:size-6" />
+                          <span className="text-white/60">{result.title}</span>
+                        </li>
+                      ))}
+                    </ul>
+                    <a href={project.link} target="_blank">
+                      <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-4 md:w-auto md:px-6">
+                        <span>View Project</span>
+                        <ArrowUpIcon className="size-4" />
+                      </button>
+                    </a>
+                  </div>
+                </div>
+
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:-mb-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,16 @@ const config: Config = {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)"],
-        serif: ["var(--font-serif)"],
+        serif: ["var(--font-calistoga)"],
+      },
+      animation: {
+        pulse: "pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0.5" },
+        },
       },
     },
   },
